f113030ccce88a8c6c9fe03cdb4593cbc148d971
jakes2255 <40160913+jakes2255@users.noreply.github.com>
2019-06-26 12:49:38 +0530

    static methods inclusion
    not specific to the object instance, but applies to the whole class

--- JsOops_es6.js
+++ JsOops_es6.js
@@ -8,7 +8,11 @@ class User{
     displayFullDetails(){
         console.log("User details are:- Name: "+this.name+" Age: "+this.age+" and Email: "+this.email);
     }
+    static displayGeneric(){
+        console.log("Hey there, this is not specific to particular Object, but to the class");
+    }
 }
 //create an object of class
 const user1 =  new User("Jeff",30,"jeff@gmail.com");//const from ES6
 user1.displayFullDetails();
+User.displayGeneric();
